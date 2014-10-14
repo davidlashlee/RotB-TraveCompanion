c8cf6e8f5506bb5b5943df845826934a91f31bd0
praburangki <praburangki@gmail.com>
2014-10-14 10:26:39 +0800

    image upload and resize

--- Gemfile
+++ Gemfile
@@ -27,6 +27,16 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem 'devise'
+
+gem 'bootstrap-sass'
+
+# for aws cloud storage
+# gem 'fog'
+# photo resizing
+gem "mini_magick"
+# file upload solution
+gem 'carrierwave'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :gender, :seeking_gender, :age)
+    params.require(:user).permit(:email, :password, :password_confirmation, :gender, :seeking_gender, :age, :image)
   end
  
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :gender, :seeking_gender, :age)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :gender, :seeking_gender, :age, :image)
   end
 end
