99ba0073dbcc7d01f73926d252eeb5a1120fb408
David Lashlee <davidlashlee@gmail.com>
2014-10-14 14:16:51 +0800

    worked on messaging function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013071106) do
+ActiveRecord::Schema.define(version: 20141013071354) do
+
+  create_table "messages", force: true do |t|
+    t.integer  "reciever"
+    t.integer  "sender"
+    t.string   "title"
+    t.string   "message_text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
