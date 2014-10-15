84f7731ffcdac441df87a1a7555e6817bd4ef01d
bob silvey <bob@mothsutdios.com>
2014-10-15 14:57:09 +0800

    schema_add

--- db/schema.rb
+++ db/schema.rb
@@ -11,54 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014063208) do
-
-  create_table "destinations", force: true do |t|
-<<<<<<< HEAD
-    t.string  "location"
-    t.string  "start_date"
-    t.string  "end_date"
-    t.integer "user_id"
-  end
-<<<<<<< HEAD
-=======
-    t.string   "location"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.integer  "user_id"
->>>>>>> f3c463ec561e96c0e1ef260827d9edc0816936f0
-=======
->>>>>>> 2d8d02f6f5f6fe94cec745456b8c826473cd0e45
-
-  create_table "messages", force: true do |t|
-    t.integer  "reciever_id"
-    t.integer  "sender_id"
-    t.string   "title"
-    t.string   "message_text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "gender"
-    t.string   "seeking_gender"
-    t.integer  "age"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+ActiveRecord::Schema.define(version: 0) do
 
 end
