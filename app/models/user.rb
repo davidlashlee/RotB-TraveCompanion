12134bdc19cba42cac10ec45077e0ea94c9eec6a
David Lashlee <davidlashlee@gmail.com>
2014-10-15 09:53:57 +0800

    got messages working I think

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
+  has_many :recievers, through: :messages
+  has_many :senders, through: :messages
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
